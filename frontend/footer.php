003d53c56a3feea793bd5423054a7453a76cd241
Savvanis Spyros <savvaniss@yahoo.gr>
2025-01-16 14:56:02 +0200

    change links

--- frontend/footer.php
+++ frontend/footer.php
@@ -1,7 +1,7 @@
 <footer>
         <p>© 2025 Cryptonow. All rights reserved.</p>
-        <a href="/index" aria-label="Home Page">Home</a> | 
-        <a href="/support" aria-label="Support Page">Support</a> | 
-        <a href="/about" aria-label="About Page">About</a> |
-        <a href="/qna" aria-label="Questions and Answers">Q&A</a>
+        <a href="/index.php" aria-label="Home Page">Home</a> | 
+        <a href="/support.php" aria-label="Support Page">Support</a> | 
+        <a href="/about.php" aria-label="About Page">About</a> |
+        <a href="/qna.php" aria-label="Questions and Answers">Q&A</a>
 </footer>
