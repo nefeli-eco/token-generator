69dfab6b452bc334d7f6559beee1abff0f448088
Savvanis Spyros <savvaniss@yahoo.gr>
2025-01-17 17:33:33 +0200

    Update footer.php

--- frontend/footer.php
+++ frontend/footer.php
@@ -3,5 +3,5 @@
         <a href="/index.php" aria-label="Home Page">Home</a> | 
         <a href="/support.php" aria-label="Support Page">Support</a> | 
         <a href="/about.php" aria-label="About Page">About</a> |
-        <a href="/legal.php" aria-label="User Agreement">About</a> |
+        <a href="/legal.php" aria-label="Terms">Terms</a> |
 </footer>
