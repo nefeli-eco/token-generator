9704cdc427abbddf20a4b3e8eb0fae14079ead7f
Savvanis Spyros <savvaniss@yahoo.gr>
2025-01-18 13:14:52 +0200

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -480,12 +480,8 @@
         <p>
   <strong>IMPORTANT:</strong> You must submit this form first. Then send
   <strong>0.05 ETH + Gas Fees</strong> to the address below <em>after</em>
-  successful submission.
-  <br>
-  <span style="color: #ff9800;">
-    (Gas fees are automatically determined by the Ethereum network at the time of your transaction.)
-  </span>
-</p>
+  successful submission.     <span style="color: #ff9800;"> (Gas fees are automatically determined by the Ethereum network at the time of your transaction.) </span>
+        </p>
           <div class="eth-address">0xE32FB3E75CA6f40682830c25e0a3C7C2A9856805</div>
           <p>Or scan the QR code:</p>
           <img
